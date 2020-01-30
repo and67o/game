70e18a0ef957fe02bea92a96181419438a6a7585
oleg <kenny_67@mail.ru>
2020-01-31 00:40:18 +0300

    add codeception

--- router.php
+++ router.php
@@ -3,15 +3,14 @@
 namespace Router;
 
 
-
 Class Router
 {
 	const PATH_CONTROLLER = '\Router\src\classes\controller\\';
-
+	
 	private static $defaultControllerName = 'MainPage';
 	private static $defaultMethodName = 'index';
 	private static $controller404 = 'PageNotFound';
-
+	
 	/**
 	 *
 	 * Роутер
@@ -19,28 +18,34 @@ Class Router
 	public function run()
 	{
 		$urlParts = explode('/', $_SERVER['REQUEST_URI']);
+		
 		//класс контроллера
 		array_shift($urlParts);
 		$controllerName = array_shift($urlParts);
 		$controllerName = $controllerName ? ucfirst($controllerName) : self::$defaultControllerName;
+		
 		//метод контроллера
 		$methodName = array_shift($urlParts);
 		$methodName = $methodName ? ucfirst($methodName) : self::$defaultMethodName;
 		$fullControllerName = self::PATH_CONTROLLER . $controllerName;
+		
 		//параметр
 		$param = count($urlParts) ? array_shift($urlParts) : '';
+		
 		if (class_exists($fullControllerName) && method_exists($fullControllerName, $methodName)) {
 			$Controller = new $fullControllerName($param);
 			$Controller->$methodName();
 			return;
 		}
+		
 		self::getPage404();
 	}
-
+	
 	/**
 	 * Показать 404
 	 */
-	public static function getPage404() {
+	public static function getPage404()
+	{
 		$fullControllerName = self::PATH_CONTROLLER . self::$controller404;
 		$Controller = new $fullControllerName();
 		$methodName = self::$defaultMethodName;
