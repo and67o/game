fe040c4e141816df343e149b47e7edfb410ce3b1
oleg <kenny_67@mail.ru>
2020-03-14 18:19:23 +0300

    change position of files

---
+++ src/classes/model/GameProcess.php
@@ -0,0 +1,69 @@
+<?php
+
+
+namespace Router\Models;
+
+use PDOException;
+
+/**
+ * Class GameProcess
+ * @package Router\Models
+ */
+class GameProcess extends Model
+{
+	
+	/**
+	 * сохранить информацию о ходе
+	 * @param array $moveParam
+	 * @return string
+	 */
+	public static function saveMove(array $moveParam)
+	{
+		$res = self::_db()
+			->table('game_process')
+			->add($moveParam);
+		if (!$res) {
+			throw new PDOException('Ход не сохранен');
+		}
+		return $res;
+	}
+	
+	/**
+	 * Получить все ходы по одной игре
+	 * @param int $gameId
+	 * @return array
+	 */
+	public static function getAllInformByGame(int $gameId) : array
+	{
+		return self::_db()
+			->select([
+				'right_count',
+				'right_position',
+				'move'
+			])
+			->table('game_process')
+			->where('g_id = ?')
+			->get([$gameId])
+			->getResult();
+	}
+	
+	/**
+	 * Использовалось ли число в игре
+	 * @param int $gameId
+	 * @param int $number
+	 * @return mixed
+	 */
+	public function isNumberUsedInThisGame(int $gameId, int $number)
+	{
+		return self::_db()
+			->select([1])
+			->table('game_process')
+			->where('g_id = ? AND move = ?')
+			->get([
+				$gameId,
+				$number
+			])
+			->getResult();
+	}
+	
+}
